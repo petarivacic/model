f41abd86c8d8bc6ef1d49c16d78e37ef271ed242
Petar Ivacic <petar.ivacic@temple.edu>
2013-09-07 01:23:30 -0400

    added bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frabbit::Application.config.secret_key_base = '0fc7ccbdfd8d96aeef9b81c0e874d012c5abd655e6f613ecc308650d99d1f7ac11ae8e9ff271b8b432ab4e360eb3d6c5763d0fe3174d755f68ff18c305fa58d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Frabbit::Application.config.session_store :cookie_store, key: '_frabbit_session'
